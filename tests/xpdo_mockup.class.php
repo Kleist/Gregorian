c7dd655009af374aeb897d03ece4d95ca199d1df
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2009-08-25 20:34:51 +0200

    Add forgotten xpdo_mockup class.

---
+++ tests/xpdo_mockup.class.php
@@ -0,0 +1,11 @@
+<?php
+class xpdo_mockup {
+    private $_mgrValidated = false;
+    
+    public function __construct() {
+    }
+    
+    public function setPackage($package,$path) {
+    	return true;
+    }
+}
