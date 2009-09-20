d85862d03f13817a3b1a2e9ee5001b5b3a47f4ef
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2009-09-20 02:12:54 +0200

    Fixing some non-issues causing notices from PHP

--- Gregorian.module.php
+++ Gregorian.module.php
@@ -13,37 +13,41 @@ $xpdo->setPackage('Gregorian', $snippetDir . 'model/');
 // $xpdo->setDebug();
 // $xpdo->setLoglevel(XPDO_LOG_LEVEL_INFO);
 
-$moduleUrl = "$_SERVER[SCRIPT_URI]?a=$_REQUEST[a]&id=$_REQUEST[id]";
+$moduleUrl = "$_SERVER[SCRIPT_NAME]?a=$_REQUEST[a]&id=$_REQUEST[id]";
+$output = '';
 $output .= "<a href='$moduleUrl&action=createContent'>Create random content</a><br />";
 $output .= "<a href='$moduleUrl&action=createTables'>Create database tables</a><br />";
 
-switch ($_REQUEST['action']) {
-    case 'createContent':
-    	// Generate 10 random events in the future
-        $calendar = $xpdo->getObject('Gregorian',1);
-    	for ($i=0;$i<10;$i++) {
-    		$start = time() + rand(1,10)*3600*24;
-    		$fields = array('summary' => "Test event number $i",
+if (array_key_exists('action',$_REQUEST)) {
+    switch ($_REQUEST['action']) {
+        case 'createContent':
+            // Generate 10 random events in the future
+            $calendar = $xpdo->getObject('Gregorian',1);
+            for ($i=0;$i<10;$i++) {
+                $start = time() + rand(1,10)*3600*24;
+                $fields = array('summary' => "Test event number $i",
                 'dtstart' => date('Y-m-d H:i',$start));
-    		if (rand(0,10)>5) $fields['dtend'] = date('Y-m-d H:i',$start+rand(1,48)*3600);
-    		$fields['allday'] = (rand(0,10)>5);
-    		if (rand(0,10)>5) $fields['description'] = "An event with a description!";
-    		if (rand(0,10)>5) $fields['location'] = "Somewhere, over the rainbow";
-    		
-    		$calendar->createEvent($fields);
-    		
-    		echo "Event '$fields[summary]' on '$fields[dtstart]' created<br />\n";
-    	}
-    	break;
-	case 'createTables':
-	   $xpdo->getManager();
-		$classes = array('Gregorian','GregorianEvent','GregorianTag','GregorianEventTag');
-		
-		foreach ($classes as $class) {
-			$output.= "Trying to create container for class '$class'... ";
-			$result = $xpdo->manager->createObjectContainer($class);
-			$output .= (($result)?"[Ok]":"[Failed]")."<br />\n";
-		}
-		break;
+                if (rand(0,10)>5) $fields['dtend'] = date('Y-m-d H:i',$start+rand(1,48)*3600);
+                $fields['allday'] = (rand(0,10)>5);
+                if (rand(0,10)>5) $fields['description'] = "An event with a description!";
+                if (rand(0,10)>5) $fields['location'] = "Somewhere, over the rainbow";
+
+                $calendar->createEvent($fields);
+
+                echo "Event '$fields[summary]' on '$fields[dtstart]' created<br />\n";
+            }
+            break;
+
+        case 'createTables':
+            $xpdo->getManager();
+            $classes = array('Gregorian','GregorianEvent','GregorianTag','GregorianEventTag');
+
+            foreach ($classes as $class) {
+                $output.= "Trying to create container for class '$class'... ";
+                $result = $xpdo->manager->createObjectContainer($class);
+                $output .= (($result)?"[Ok]":"[Failed]")."<br />\n";
+            }
+            break;
+    }
 }
 return $output;
