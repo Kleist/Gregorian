050987d8d41658cf0d9c3046fc238ebe45ac97ed
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2009-09-30 22:08:31 +0200

    White space fixes

--- Gregorian.snippet.php
+++ Gregorian.snippet.php
@@ -1,31 +1,31 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * TODO Save button in edit form doesn't work as desired.
  * TODO Create manager module
  * TODO Translate to danish (and make translateable)
  * TODO Update parameter list
- *  
+ *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Snippet parameters:
  * calId        - Id number of calendar (set if more than one on site)  default: 1
- * 
+ *
  * adminGroup   - Name of webgroup that can edit calendar               default: ''
  * mgrIsAdmin   - All users logged in to the manager can edit calendar  default: 1
- * 
+ *
  * template     - Name of the template to use                           default: 'default'
  * lang         - Language code                                         default: 'en'
  * formatForICal- Format dates for iCal                                 default: 0
- * 
+ *
  * count        - Number of calendar items to show per page             default: 10
  * TODO view    - (option to show items in other ways than 'agenda' aka 'list')
- * 
+ *
  * AJAX-related. (Not fully implemented yet!)
  * ajaxId      - Id of the ajax processor document (the document with ajax=`1` snippet call) (default: 0)
  * ajax		   - This is the ajax-processor snippet call, (default: 0)
  * calId       - Id of the calendar-document, used in the ajax-processor snippet call. (default: 0)
- * 
+ *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 if (!is_object($modx)) die("You shouldn't be here!");
 
@@ -40,7 +40,7 @@ $mgrIsAdmin =   (isset($mgrIsAdmin))    ?  $mgrIsAdmin      : true;
 
 $template =     (isset($template)) 		? $template         : 'default';
 $lang =         (isset($lang)) 			? $lang             : 'en';
-if (isset($_REQUEST['lang']) && in_array($_REQUEST['lang'], array('da','en'))) 
+if (isset($_REQUEST['lang']) && in_array($_REQUEST['lang'], array('da','en')))
     $lang = $_REQUEST['lang'];
 $formatForICal =(isset($formatForICal)) ? $formatForICal    : 0;
 
@@ -79,7 +79,7 @@ else {// this is the main doc snippet call
 $ajaxEnabled = ($ajaxUrl!==NULL);
 
 // Load xPDO
-$xpdo = new xPDO(XPDO_DSN, XPDO_DB_USER, XPDO_DB_PASS, XPDO_TABLE_PREFIX, 
+$xpdo = new xPDO(XPDO_DSN, XPDO_DB_USER, XPDO_DB_PASS, XPDO_TABLE_PREFIX,
 	array (PDO_ATTR_ERRMODE => PDO_ERRMODE_WARNING, PDO_ATTR_PERSISTENT => false, PDO_MYSQL_ATTR_USE_BUFFERED_QUERY => true));
 $xpdo->setPackage('Gregorian', $snippetDir . 'model/');
 // $xpdo->setDebug();
@@ -174,8 +174,8 @@ $fields = array('summary','description','location','allday');
 if ($action == 'savetag') {
 	// Check if tag exists
 	$tag = $xpdo->getObject('GregorianTag',array('tag'=>$_POST['tag']));
-	
-	if ($tag != NULL) { 
+
+	if ($tag != NULL) {
 		infoMessage('tag_exists',$tag->get('tag'));
 		$action = 'view';
 	} else {
@@ -197,7 +197,7 @@ if ($action == 'tagform') {
     $output .= $calendar->replacePlaceholders(
         $calendar->_template['tagform'],
         array(
-            'action'=>'savetag', 
+            'action'=>'savetag',
             'formAction' => $calendar->createUrl(),
             'addTagText' => $calendar->lang('add_tag'),
             'tagNameText' => $calendar->lang('tag_name'),
@@ -210,9 +210,9 @@ if ($action == 'tagform') {
 if ($action == 'save') {
 	//	TODO infoMessage("Why is '->save()' not done with CreateEvent()?");
 	//	TODO Validate input (not empty summary, not empty start date)
-	
+
 	$e_fields = array();
-	
+
 	$saved = false;
 	$valid = true;
 
@@ -222,7 +222,7 @@ if ($action == 'save') {
 	}
 	// Make allday boolean
 	$e_fields['allday'] = isset($e_fields['allday']);
-	
+
 	// Check required fields, stop 'save' if they are not adequate
 	// TODO Better date-validation
 	// TODO Make validation on all fields, not just required, perhaps with xPDO's built in validation-features
@@ -234,7 +234,7 @@ if ($action == 'save') {
 		errorMessage('error_summary_required');
 		$valid = false;
 	}
-	
+
 	// Create datetime for start and end, append time if not allday
 	$e_fields['dtstart'] = $_POST['dtstart'];
 	$e_fields['dtend'] = $_POST['dtend'];
@@ -270,7 +270,7 @@ if ($action == 'save') {
 			}
 		}
 	}
-	
+
 	// TODO Add more validation here (dtstart < dtend, ???
 	if ($valid) {
 		// Save edited event / Create event
@@ -296,7 +296,7 @@ if ($action == 'save') {
 		}
 	}
 	else { // Not valid
-		$action = 'showform';	
+		$action = 'showform';
 	}
 } // action 'save'
 
@@ -305,7 +305,7 @@ if ($action == 'showform') {
 	$gridLoaded = false;
 	$e_ph = array_flip($fields);
 
-	if (isset($e_fields)) { 	
+	if (isset($e_fields)) {
 		$e_ph = $e_fields;
 		$gridLoaded = true;
 		// TODO Export this to a function
@@ -325,7 +325,7 @@ if ($action == 'showform') {
 		// Populate placeholders if editing event
 		$e_ph = $event->get($fields);
 		foreach ($e_ph as $key => $value) $e_ph[$key] = $value;
-		
+
         // TODO Export this to a function
 		$e_ph['dtstart'] = substr($event->get('dtstart'),0,10);
 		$e_ph['dtend'] = substr($event->get('dtend'),0,10);
@@ -338,7 +338,7 @@ if ($action == 'showform') {
 			$e_ph['tmstart'] = substr($event->get('dtstart'),11,5);
 			$e_ph['tmend'] = substr($event->get('dtend'),11,5);
 		}
-		
+
 		$e_tags = $event->getMany('Tags');
 		$e_tag_ids = array();
 		foreach ($e_tags as $tag) {
@@ -350,10 +350,10 @@ if ($action == 'showform') {
 		errorMessage('error_event_doesnt_exist',$eventId);
 		$reloadCal = true;
 		$action = 'view';
-	}					
-	else {	
+	}
+	else {
 		// Start with default form if new event
-		foreach($fields as $field) 
+		foreach($fields as $field)
 			$e_ph[$field] = '';
 		$e_ph['allday']=true;
 		$gridLoaded = true;
@@ -391,7 +391,7 @@ if ($action == 'showform') {
 
 			$e_ph['tagCheckboxes'] .= $calendar->replacePlaceholders($calendar->_template['formCheckbox'], array('name'=>$cleanTagName,'label'=>$tagName,'checked'=>$checked));
 		}
-		
+
         $langPhs = array(
             'editEventText'     =>  'edit_event',
             'summaryText'       =>  'summary',
@@ -430,7 +430,7 @@ if ($action == 'delete') {
 		if (is_object($event)) {
 			$summary = $event->get('summary');
 			$deleted = $event->remove();
-		} 
+		}
 
 		if ($deleted) {
 			infoMessage('event_deleted',$summary);
@@ -449,7 +449,7 @@ if ($action == 'view') {
 	$cal = NULL;
 	if (isset($reloadCal) && $reloadCal === true) {
 		$cal = $calendar->xpdo->getObject('Gregorian',$calendar->get('id'));
-		if ($cal === NULL) {						
+		if ($cal === NULL) {
 			errorMessage('Could not load calendar with id '.$calendar->get('id'));
 		}
 		else {
