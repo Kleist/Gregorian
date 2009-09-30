1b4e02cedbaad677f4241c56d20f2e4cacb44d65
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2009-09-30 22:06:28 +0200

    Adding Selenium TestCase for CreateEvent

---
+++ tests/TestCreateEvent.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+    function setUp()
+    {
+        $this->setBrowser("*chrome");
+        $this->setBrowserUrl("http://localhost/");
+    }
+
+
+    function testAdminButtonsAvailable()
+    {
+        $this->login();
+        $this->open("/");
+        $this->assertTrue($this->isElementPresent("link=Create entry"));
+        $this->assertTrue($this->isElementPresent("link=Add tag"));
+    }
+
+    function testRequiredFields()
+    {
+        $this->login();
+        $this->open("/");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Create entry");
+        $this->waitForPageToLoad("30000");
+        $this->click("submit");
+        $this->waitForPageToLoad("30000");
+        $this->assertTextPresent("Edit event");
+        $this->verifyTextPresent("Start date required");
+        $this->verifyTextPresent("Summary required");
+    }
+
+    function testTagMemoryOnError()
+    {
+        $this->addTestTag();
+        $this->open("/?action=showform");
+        $this->click("TestTag");
+        $this->clickAndWait("submit");
+        $this->assertValue('TestTag','on');
+    }
+
+    function testLogin()
+    {
+        $this->login();
+        $this->assertTrue($this->isElementPresent("link=Logout"));
+    }
+
+    function testAddTestTag()
+    {
+        $this->addTestTag();
+        $this->open("/?action=showform");
+        $this->assertTextPresent("TestTag");
+    }
+
+    function login()
+    {
+        $this->open("/");
+        $this->click("link=WebLogin");
+        $this->waitForPageToLoad("30000");
+        $this->type("username", "caladmin");
+        $this->type("password", "caladmin");
+        $this->click("cmdweblogin");
+        $this->waitForPageToLoad("30000");
+    }
+
+    function addTestTag()
+    {
+        $this->login();
+        $this->open("/?action=tagform");
+        $this->type("tag","TestTag");
+        $this->click('submit');
+    }
+}
+?>
